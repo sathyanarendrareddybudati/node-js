2af9837015b82f3eb05d6461dff00469324b1d96
sathyanarendrareddybudati <sathyanarendrareddybudati@gmail.com>
2023-06-15 19:29:42 +0530

    changes in mongoosecrud

--- mongooseCrud.js
+++ mongooseCrud.js
@@ -1,21 +1,49 @@
 const express = require("express");
 const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost:27017/beebro");
 
+const productSchema = new mongoose.Schema({
+    name: String,
+    gender: String
+});
 
 const main = async () => {
-    await mongoose.connect("mongodb://localhost:27017/beebro");
-    const productSchema = new mongoose.Schema({
-        name: String,
-        gender: String
-    });
     let productModel = mongoose.model("items", productSchema)
-    let data = new productModel({ name: "dulal", gender: "male" });
+    let data = new productModel({ name: "nava", gender: "male" });
     let result = await data.save();
     console.log("....result", result);
 }
 
-main();
+// main();
 
+const updatemain = async () => {
+    let productModel = mongoose.model("items", productSchema)
+    let data = await productModel.updateOne(
+        {name:"nava"},
+        {$set:{name:"Nava"}}
+    )
+    console.log(data)
+}
+
+// updatemain();
+
+const deletemain = async () => {
+    let productModel = mongoose.model("items", productSchema)
+    let data = await productModel.deleteOne(
+        {name:"Nava"}
+    )
+    console.log(data)
+}
+
+// deletemain();
+
+const findmain = async () => {
+    let productModel = mongoose.model("items", productSchema)
+    let data = await productModel.find()
+    console.log(data)
+}
+
+findmain();
 
 const PORT = process.env.port || 4000;
 const app = express()
